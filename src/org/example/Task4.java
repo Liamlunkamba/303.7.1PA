93d816c2551c7a3d7d33be606aa229c5494b42c6
Liamlunkamba <liamlunkamba12@gmail.com>
2024-07-14 17:26:23 -0400

    commites urgggg

---
+++ src/org/example/Task4.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import javax.xml.transform.Source;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] Arr={1,34,6,8,7};
+        System.out.println(Arr[0]);
+        System.out.println(Arr[5]);
+        System.out.println(Arr[-1]);
+    }
+}
